fd7fa89159769062b82af2b58fce06a59e34b1c4
Marcelo Lanegra <Mlanegra@megalabs.com.pe>
2022-06-24 15:41:16 -0500

    final push 2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
-import handleSubmit from './form.js'
+const $form = document.querySelector('#contact')
+// const $buttonMailto = document.querySelector('#mailto')
 
-handleSubmit()
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  // $buttonMailto.setAttribute('href',`mailto:mlanegra@megalabs.com.pe?subject=${form.get('name')} - Correo: ${form.get('mail')}&body=${form.get('message')}`)
+  // $buttonMailto.click()
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  if (response.ok) {
+    this.reset()
+    alert('Gracias por contactarme')
+  }
+}
